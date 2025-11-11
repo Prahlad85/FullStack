10fda3d5bf7f9e3f9108f8fe39dd6fa9f561e068
Prahlad85 <prahladkr21@gmail.com>
2025-11-11 22:07:25 +0530

    upload all Experiments

---
+++ script.js
@@ -0,0 +1,10 @@
+let age = prompt("Enter your age:");
+
+age = Number(age);
+
+if (age >= 18) {
+  alert("You are allowed to drive a car ");
+} else {
+  alert("Sorry, you are not allowed to drive a car ");
+}
+
